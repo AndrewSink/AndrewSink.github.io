4773f37e62682a677d0c0806bb513f5fd0051b62
AndrewSink <46334898+AndrewSink@users.noreply.github.com>
2021-11-04 18:05:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+
 console.log('Hello World!')
